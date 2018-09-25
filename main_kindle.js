fda0797c044e20ad0cfe432ad654d397d5951d05
gahubaul <gautier.hubault@gmail.com>
2018-09-25 20:36:22 +0200

    page test and query request

--- main_kindle.js
+++ main_kindle.js
@@ -1,27 +1,52 @@
-const express = require('express');
+const express = require('express')
 //var session = require('cookie-session');
-const multer = require('multer');
+const multer = require('multer')
 
-const upload = multer();
+const upload = multer()
 //var upload = multer({ dest: 'uploads/' });
-const app = express();
+const app = express()
 
 //var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
-
+function generateHTML (res) {
+    const titi = {
+        status: '',
+        error: '',
+        successs: ''
+    }
+    
+    if (res === 'salut')
+    {
+        titi.status = 200
+        titi.successs = 'Tout a fonctionné'
+        titi.error = null
+    } else {
+        titi.status = 404
+        titi.successs = null
+        titi.error = 'DATA NOT FOUND'
+    }
+    return (titi)
+}
 
 //app.use(session({secret: 'todotopsecret'}));
 
 app.get('/', function (req, res) {
-    res.render('page.ejs');
+    console.log(req)
+    res.render('page.ejs')
+})
+
+app.get('/test', function (req, res) {
+    const toto = generateHTML(req.query.var1)
+    res.send(toto)
+    console.log('Voila ma couille')
 })
 
 app.post('/upload', upload.none(), function (req, res, next) {
-    res.render('page_upload.ejs');
-    console.log(req.file); 
-    console.log(req.body); 
+    res.render('page_upload.ejs')
+    console.log(req.file)
+    console.log(req.body)
 })
 //un fichier ne peut pas être passé en GET, qui correspond à l'encodage de texte dans l'URL. On utilise donc POST
 // aller voir https://scotch.io/tutorials/express-file-uploads-with-multer
