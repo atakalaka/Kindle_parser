891337645b758684413d539926477484a991fc63
atakalaka <43369422+atakalaka@users.noreply.github.com>
2018-09-27 00:26:38 +0200

    ajout d'une page de parsing et des fonctins correspondantes

--- main_kindle.js
+++ main_kindle.js
@@ -2,26 +2,32 @@ const express = require('express')
 //var session = require('cookie-session');
 const multer = require('multer')
 
+const fs = require('fs')
+
 const fileUpload = require('express-fileupload');
 
 const upload = multer()
-//var upload = multer({ dest: 'uploads/' });
+
 const app = express()
 
+const DOMParser = require('dom-parser')
+
+const parser = new DOMParser()
+
 const port = process.env.PORT || 8080;
+
 
 //var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-function generateHTML (res) {
+function generateHTML(res) {
     const titi = {
         status: '',
         error: '',
         successs: ''
     }
 
-    if (res === 'salut')
-    {
+    if (res === 'salut') {
         titi.status = 200
         titi.successs = 'Tout a fonctionné'
         titi.error = null
@@ -42,36 +48,42 @@ app.get('/', function (req, res) {
     res.render('page.ejs')
 })
 
-// app.get('/tab', function(req, res) {
-//     var toBeParsed=Notes Sapiens.getElementsByClassName('noteText')
-// })
+app.post('/parsed', function (req, res) {
+    console.log(__dirname + '/Notes-unparsed/' + 'Notes Sapiens.html')
+    fs.readFile(__dirname + '/Notes-unparsed/' + 'Notes Sapiens.html', 'utf8', function (err, data) {
+        if (err) throw err;
+
+         //transformer le texte en objet html
+        const valueHTML = parser.parseFromString(data, 'text/html')
+        const tab = valueHTML.getElementsByClassName('noteText')
+        console.log(tab)//marche po
+        // res.send(tab[0])
+    });
+})
+
+
 
 app.get('/test', function (req, res) {
     const toto = generateHTML(req.query.var1)
-    // res.send(process.env)
-    res.send(toto)
-    console.log('Voila ma couille')
 })
 
-app.post('/upload', function(req, res) {
-    console.log(req)
-    console.log(req.read)
+app.post('/upload', function (req, res) {
+    // console.log(req)
+    // console.log(req.read)
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let sampleFile = req.files.file_link;
-// quelque chose
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('/Notes-unparsed/', function(err) {
-        if (err)
-        return res.status(500).send(err);
-
-        res.send('File uploaded!');
+    sampleFile.mv(__dirname + '/Notes-unparsed/' + sampleFile.name, function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.render('fileuploaded.ejs');
     })
 })
 
-//un fichier ne peut pas être passé en GET, qui correspond à l'encodage de texte dans l'URL. On utilise donc POST
-// aller voir https://scotch.io/tutorials/express-file-uploads-with-multer
+
 
 app.listen(port);
