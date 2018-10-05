26daba5424fb426ee70bae0ee76bd1281067bb81
atakalaka <43369422+atakalaka@users.noreply.github.com>
2018-10-06 01:17:50 +0200

    J'ai supprimé la page du milieu (fileuploaded) qui ne servaiot à rien ; j'ai tout mis dans post upload

--- main_kindle.js
+++ main_kindle.js
@@ -8,84 +8,49 @@ const parser = new DOMParser()
 const port = process.env.PORT || 8080;
 
 
-function generateHTML(res) {
-    const titi = {
-        status: '',
-        error: '',
-        successs: ''
-    }
-
-    if (res === 'salut') {
-        titi.status = 200
-        titi.successs = 'Tout a fonctionné'
-        titi.error = null
-    } else {
-        titi.status = 404
-        titi.successs = null
-        titi.error = 'DATA NOT FOUND'
-    }
-    return (titi)
-}
-
 //app.use(session({secret: 'todotopsecret'}));
 
 app.use(fileUpload());
 
 app.get('/', function (req, res) {
     app.use(express.static(__dirname + '/public')); // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
-    res.render('page.ejs')
+    res.render('page.ejs') //page.ejs renvoie sur /upload
 })
 
-//page.ejs renvoie sur /upload ; soit fileuploaded.ejs
-app.post('/upload', function (req, res) {
-    if (!req.files)
-        return res.status(400).send('No files were uploaded.');
 
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+app.post('/upload', function (req, res) {
+    // var vocab = {};
+    // var quotes = [];
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    }
     let sampleFile = req.files.file_link;
     // Use the mv() method to place the file somewhere on your server
     sampleFile.mv(__dirname + '/Notes-unparsed/' + sampleFile.name, function (err) {
         if (err) {
             return res.status(500).send(err);
         }
-        res.render('fileuploaded.ejs');
+        // res.render('fileuploaded.ejs');
+    })
+
+    fs.readFile(__dirname + '/Notes-unparsed/' + sampleFile.name, 'utf8', function (err, data) {
+        if (err) throw err;
+        //transformer le texte en objet html
+        const valueHTML = parser.parseFromString(data);
+        const tab = valueHTML.getElementsByClassName("noteText");
+        var vocab = tab;
+        var quotes = [];
+        for (let j = 0; j < vocab.length; j++) {
+            // si la note fait plus de 5 mots, on considère que c'est une citation
+            if (vocab[j].innerHTML.split(" ").length > 5) {
+                quotes.push(vocab[j])
+                vocab.splice(j, 1);
+                j--
+            }
+        }
+        res.status(200);
+        res.render('note_parsed.ejs', { quotesejs: quotes, vocabejs: vocab });
     })
 })
-
-app.post('/parsed', function (req, res) {
-    // console.log(__dirname + '/Notes-unparsed/' + 'Notes Sapiens.html')
-    var all_note_files = fs.readdirSync(__dirname + '/Notes-unparsed/');
-    // for (var i = 0; i < all_note_files.length; i++) {
-    // console.log(all_note_files[i])
-    // }
-
-    var vocab = [];
-    var quotes = [];
-    all_note_files.forEach(function (file_on_server) {
-        fs.readFile(__dirname + '/Notes-unparsed/' + file_on_server, 'utf8', function (err, data) {
-            if (err) throw err;
-
-            //transformer le texte en objet html
-            const valueHTML = parser.parseFromString(data);
-            const tab = valueHTML.getElementsByClassName("noteText");
-            if (tab.length !== 0) { //si on ne trouve pas de noteText, on passe au fichier suivant
-                vocab = tab;
-                quotes = [];
-                for (i = 0; i < vocab.length; i++) {
-                    // si la note fait plus de 5 mots, on considère que c'est une citation
-                    if (vocab[i].innerHTML.split(" ").length > 5) {
-                        quotes.push(vocab[i])
-                        vocab.splice(i, 1);
-                        i--
-                    }
-                }
-
-            }
-        });
-    }); 
-    res.status(200);
-    res.render('note_parsed.ejs', { quotesejs: quotes, vocabejs: vocab });
-})
-
 
 app.listen(port);
