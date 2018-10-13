3c8ec1cedc7f93858761bae4655cf1c6a72ef33f
Arthur Goldstein <43369422+atakalaka@users.noreply.github.com>
2018-10-13 17:05:19 +0200

    Arthur (#6)
    * ajout d'un commentaire
    
    * ajout d'une page de parsing et des fonctins correspondantes
    
    * took off the spaces between the parameters declarations
    
    * removed useless requires, created two objects containing quotes and vocabulary, removed useless comments
    
    * indentation et correction pour charger le CSS dans la page de rendu final
    
    * added a system to be able to read different documents in the "Notes-unparsed" directory
    
    * Worked on the mutli-file problem
    
    * J'ai supprimé la page du milieu (fileuploaded) qui ne servaiot à rien ; j'ai tout mis dans post upload
    
    * Ajouté la titre et supprimé quelques comm inutiles
    
    * passé de 5 à 6 mots pour les citations
    
    * Added a 404 status page
    
    * Added nodemon to package.json, a view engine, and "/assets" alias
    
    * corrected a bug on json file
    
    * Turned vocab list into a table with a "translation" column
    
    * Changed the style of the table

--- main_kindle.js
+++ main_kindle.js
@@ -1,22 +1,24 @@
 const express = require('express')
-//var session = require('cookie-session');
+const path = require('path')
 const fs = require('fs')
 const fileUpload = require('express-fileupload');
 const app = express()
 const DOMParser = require('dom-parser')
 const parser = new DOMParser()
 const port = process.env.PORT || 8080;
+//var session = require('cookie-session');
+// const lib = require('./server/module') //code de Gautier
 
 //app.use(session({secret: 'todotopsecret'}));
 
+app.set('view engine', 'ejs') //permet de ne pas ecrire l'extension "ejs"
 
-app.use(fileUpload())
-app.use(express.static(__dirname + '/public'))
-console.log(__dirname)
+app.use(fileUpload());
+app.use('/assets', express.static('public')) //crée un alias de public qui permet d'aller chercher dans public avec les doc ejs
 
+
+// Routes
 app.get('/', function (req, res) {
-
-    app.use(express.static(__dirname + '/public')); // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
     res.render('page.ejs') //page.ejs renvoie sur /upload
 })
 
@@ -26,33 +28,46 @@ app.post('/upload', function (req, res) {
         return res.status(400).send('No files were uploaded.');
     }
     let sampleFile = req.files.file_link;
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(__dirname + '/Notes-unparsed/' + sampleFile.name, function (err) {
-        if (err) {
-            return res.status(500).send(err);
-        }
-    })
 
-    fs.readFile(__dirname + '/Notes-unparsed/' + sampleFile.name, 'utf8', function (err, data) {
-        if (err) throw err;
-        //transformer le texte en objet html
-        const valueHTML = parser.parseFromString(data);
-        const tab_words = valueHTML.getElementsByClassName("noteText");
-        const title = valueHTML.getElementsByClassName("bookTitle"); //charge le titre du livre 
-        var vocab = tab_words;
-        var quotes = [];
-        for (let j = 0; j < vocab.length; j++) {
-            // si la note fait plus de 6 mots, on considère que c'est une citation
-            if (vocab[j].innerHTML.split(" ").length > 6) {
-                quotes.push(vocab[j])
-                vocab.splice(j, 1);
-                j--
+    if (sampleFile) {
+        sampleFile.mv(__dirname + '/Notes-unparsed/' + sampleFile.name, function (err) {
+            if (err) {
+                return res.status(500).send(err);
             }
-        }
-        res.status(200);
-        res.render('note_parsed.ejs', { quotesejs: quotes, vocabejs: vocab, titleejs: title });
+        })
 
-    })
+        fs.readFile(__dirname + '/Notes-unparsed/' + sampleFile.name, 'utf8', function (err, data) {
+            if (err) { return res.status(400).send("No files found on the server") }
+            //transformer le texte en objet html
+            const valueHTML = parser.parseFromString(data);
+            const tab_words = valueHTML.getElementsByClassName("noteText");
+            const title = valueHTML.getElementsByClassName("bookTitle"); //charge le titre du livre 
+            var vocab = tab_words;
+            var quotes = [];
+            for (let j = 0; j < vocab.length; j++) {
+                // si la note fait plus de 6 mots, on considère que c'est une citation
+                if (vocab[j].innerHTML.split(" ").length > 6) {
+                    quotes.push(vocab[j])
+                    vocab.splice(j, 1);
+                    j--
+                }
+            }
+            res.status(200);
+            res.render('note_parsed.ejs', {
+                quotesejs: quotes,
+                vocabejs: vocab,
+                titleejs: title
+            });
+        })
+        fs.unlinkSync(__dirname + '/Notes-unparsed/' + sampleFile.name)//supprime le fichier une fois affiché
+    } else {
+        return res.status(400).send('No files were uploaded.')
+    }
 })
+
+app.get('*', function (req, res) {
+    res.status(404).render('404notfound.ejs')
+});
+
 
 app.listen(port);
